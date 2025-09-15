08ad36119aa0d8633b4c7d2dbc9519888954c955
Heber Gomez <heber12398@gmail.com>
2025-09-15 19:13:52 -0300

    Cambios tp6

---
+++ src/modelo/Rubro.java
@@ -0,0 +1,12 @@
+package modelo;
+
+/**
+ *
+ * @author Nehuen
+ */
+public enum Rubro {
+    COMESTIBLE, LIMPIEZA, PERFUMERIA
+}
+//correccion hacere una clase normal con un arreglo y guardar las 3 variantes ahi 
+//o dejar esta que es una clase enum donde guarda las 3 variables y tambien cumple la funcion 
+//pero no todos la conocen
